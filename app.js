7957836249719a8d458ffe8d9b6dfacb5eff2ef4
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-07 22:06:38 +0800

    add: count up

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ class Kaunto extends HTMLElement {
   constructor() {
     super();
     const className = this.getAttribute("class");
-    const tag = this.getAttribute("tag");
+    const count = this.getAttribute("to");
 
     const template = document.createElement("template");
-    template.innerHTML = ``;
+    template.innerHTML = `<span class="${className}">${count}</span>`;
 
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    let i = 0;
+    const timer = setInterval(() => {
+      ++i;
+      if (i === parseInt(count)) clearInterval(timer);
+      this.shadowRoot.children[0].innerText = i;
+    }, 0.00001);
   }
 }
+
+window.customElements.define("kaunto-up", Kaunto);
